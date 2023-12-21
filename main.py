034c9c11bd05f7d1279676c094d785b88d6856c1
AkiTheMemeGod <k.akashkumar@gmail.com>
2023-12-21 11:11:14 +0530

    added a "add new todo " feature to the webpage

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import streamlit as st
 from functions import *
+todos = get_todos()
+
+
+def add_todo():
+    todo = st.session_state["new_todo"]
+    todos.append(todo + "\n")
+    put_todos(todos)
+    st.session_state["new_todo"] = None
+
 
 st.title("Todo App")
 st.subheader("Made in python by akash")
 st.write("Click on the checkboxes to complete the todo")
-todos = get_todos()
 for i in todos:
     st.checkbox(i)
 
-x = st.text_input(label="Enter a Todo below", placeholder="Remind be about...")
-print(x)
+st.text_input(label="Enter a Todo below", placeholder="Remind be about...",
+                  on_change=add_todo, key="new_todo")
