3c1c3714d369fe72569221f490b644bf832a0e05
AkiTheMemeGod <k.akashkumar@gmail.com>
2023-12-21 11:21:37 +0530

    added a complete feature

--- main.py
+++ main.py
@@ -13,8 +13,13 @@ def add_todo():
 st.title("Todo App")
 st.subheader("Made in python by akash")
 st.write("Click on the checkboxes to complete the todo")
-for i in todos:
-    st.checkbox(i)
+for index, i in enumerate(todos):
+    box = st.checkbox(i, key=i)
+    if box:
+        todos.pop(index)
+        put_todos(todos)
+        del st.session_state[i]
+        st.rerun()
 
 st.text_input(label="Enter a Todo below", placeholder="Remind be about...",
                   on_change=add_todo, key="new_todo")
